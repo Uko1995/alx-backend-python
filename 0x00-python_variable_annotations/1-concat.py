12fb668a6ba3d1c8c202417e5f3c697b35cd169a
Uko Uwatt <uwattuko@gmail.com>
2024-07-05 05:31:29 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' function that takes 2 strings and concatenates it to a string '''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concatenates 2 strings'''
+    return str1 + str2
