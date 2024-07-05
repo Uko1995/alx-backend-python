69faf01cafa7de6f7e110492b8608341d87095d6
Uko Uwatt <uwattuko@gmail.com>
2024-07-05 07:14:26 +0100

    task 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/urs/bin/env python3
+'''annotate the function below'''
+from typing import Union, Any, Mapping, TypeVar
+T = TypeVar('T')
+
+
+def safely_get_value(
+                     dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
