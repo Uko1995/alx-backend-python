607bca929d430b1e43d36a608245be36c5a66448
Uko Uwatt <uwattuko@gmail.com>
2024-07-05 05:35:49 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''function takes a float and returns the floor of the float'''
+import math
+
+
+def floor(n: float) -> int:
+    '''function returns the floor of a float'''
+    return math.floor(n)
