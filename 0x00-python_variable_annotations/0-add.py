879f5a24f756703b1380e217b2e6d3edfb91b4b4
Uko Uwatt <uwattuko@gmail.com>
2024-07-05 05:26:50 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''write a type-annotated function that takes floats as arguments and returns
+    sum as float'''
+
+
+def add(a: float, b: float) -> float:
+    ''' takes 2 floats and retuens their sum as float '''
+    return a + b
