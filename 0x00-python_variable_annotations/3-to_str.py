b6155be61f2c4900875fd03990de87b9d66a1c06
Uko Uwatt <uwattuko@gmail.com>
2024-07-05 05:41:20 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''function takes a float and returns a string representation of the float'''
+
+
+def to_str(n: float) -> str:
+    '''returns a string representatio of a n'''
+    return str(n)
