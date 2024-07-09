9242988cd596d9400890de30d117d87bc50a84c5
Uko Uwatt <uwattuko@gmail.com>
2024-07-09 12:50:22 +0100

    task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 '''create async coroutine that uses async comprehension'''
-import random
 import asyncio
-from typing import Generator, List
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> List[float]:
     '''async_comprehension coroutine'''
     numbers = [num async for num in async_generator()]
     return numbers
