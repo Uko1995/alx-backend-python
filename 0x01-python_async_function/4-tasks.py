3eeedeee03a15a6282934cfb81d195ad645e867c
Uko Uwatt <uwattuko@gmail.com>
2024-07-09 04:55:27 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,15 +4,15 @@ import asyncio
 import heapq
 from typing import List
 
-# Import the wait_random function from the previous task
-wait_random = __import__('0-basic_async_syntax').wait_random
+# Import the task_wait_random function from the previous task
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Wait_n function'''
     heap = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
     # Run all tasks concurrently and collect results
     for task in asyncio.as_completed(tasks):
         delay = await task
