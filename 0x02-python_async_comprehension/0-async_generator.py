c28fdd1f6a0c8bd73c21928255926787aca04dac
Uko Uwatt <uwattuko@gmail.com>
2024-07-09 12:32:57 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     '''async_generator function'''
-    for i in range(10):
+    for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
