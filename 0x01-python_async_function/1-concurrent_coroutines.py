7a51c66827a6f9715ea2bed2696ac9848d67eae3
Uko Uwatt <uwattuko@gmail.com>
2024-07-09 11:14:04 +0100

    corrected task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Wait_n function'''
     heap = []
     tasks = [wait_random(max_delay) for _ in range(n)]
-    
+
     # Run all tasks concurrently and collect results
     for task in asyncio.as_completed(tasks):
         delay = await task
