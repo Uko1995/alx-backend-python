bbd390657dd6baa4904d10fc421fb8ecd23fb94d
Uko Uwatt <uwattuko@gmail.com>
2024-07-09 03:31:32 +0100

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr?bin/env python3
+''' runs wait_random() n times and returns a list of the delays'''
+import random
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''wait_n function'''
+    tasks = []
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+
+    result = await asyncio.gather(*tasks)
+    return sorted(result)
