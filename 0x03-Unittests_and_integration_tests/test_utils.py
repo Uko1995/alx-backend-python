450ebb4f389208a97fa28b29fba2fc913036a78d
Uko Uwatt <uwattuko@gmail.com>
2024-07-30 03:14:06 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,9 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         '''testing the access_nested_map function'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(self, nested_map, path):
+        '''testing for KeyError'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
