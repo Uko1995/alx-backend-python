2ad868aef7b9ae69d76f6c5195ba27bad7194594
Uko Uwatt <uwattuko@gmail.com>
2024-07-30 03:48:47 +0100

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -40,3 +40,26 @@ class TestGetJson(unittest.TestCase):
         test = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(test, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''testing the memoize function'''
+    def test_memoize(self):
+        '''testing memoize function'''
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            test_obj = TestClass()
+
+            r1 = test_obj.a_property
+            r2 = test_obj.a_property
+            self.assertEqual(r1, 42)
+            self.assertEqual(r2, 42)
+            mock_method.assert_called_once()
